0794ebe1eab7ece9337228c3c2a79e9a227d7cd9
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2021-03-15 22:05:42 -0500

    bubble sort final

--- algoexpert/bubblesort.js
+++ algoexpert/bubblesort.js
@@ -1,15 +1,16 @@
 const bubbleSort = (array) => {
-    let swaps = 0
-    let helperFunction = (endPoint) => {
-        for (let i = 0; i < array.length; i ++) {
-            let a = array[i]
-            let b = array[i+1]
-            if (a > b) {
-                array[i] = b
-                array[i+1] = a
-                swaps++
+    let isSorted = false
+    let counter = 0
+    while (!isSorted) { //!isSorted === (isSorted === false) <-- this statement is comparing "True" to truthy
+        isSorted = true
+        for (let i = 0; i < array.length - 1 - counter; i ++) {
+            if (array[i] > array[i+1]) {
+                array[i] = array[i+1]
+                array[i+1] = array[i]
+                isSorted = false
             }
         }
+        counter ++
     }
-    
+    return array
 }
